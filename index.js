b529fdf1c401181898f8b0806343a0ca06d3b89a
Anthony Pena <apena5@alumni.jh.edu>
2021-08-27 12:37:44 -0400

    initial files structure

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+// MARKDOWN NEEDS
+// - title of project
+// - description
+// - Table of Contents
+// - Installation
+// - Usage
+// - License
+//      - Badge
+// - Contributin
+// - Test
+// - Questions
+//      - GitHub Username
+
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generatePage = require('./src/markdown-template.js');
+// const { writeFile, copyFile } = require('./utils/generate-markdown');
+const generateMarkdown = require('./utils/generate-markdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
